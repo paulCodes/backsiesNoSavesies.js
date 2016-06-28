35d4dbbff7682083c93216ab09225b2c5ab274a5
Paul Parker <pparker@jcasolutions.com>
2016-06-28 00:17:23 -0600

    add debug and add ignore selectors

--- backsiesNoSavesies.js
+++ backsiesNoSavesies.js
@@ -1,5 +1,5 @@
 /*
- * backsiesNoSavesies.js 0.0.1
+ * backsiesNoSavesies.js 0.0.2
  * Paul Parker, http://github.com/paulCodes
  * backsiesNoSavesies.js is open sourced under the MIT license.
  * Portions of backsiesNoSavesies.js are inspired by Alphamale.
@@ -7,14 +7,17 @@
  */
 (function(window, document, undefined) {
 
-	var BacksiesNoSavesies = function(changeSelectors, saveSelectors, message) {
-		this._unsaved = false;
-		this._message = message || "Warning: You have unsaved changes on this page. If you leave your changes will be lost.";
-		this._changeSelectors = changeSelectors || [":input"];
-		this._saveSelectors = saveSelectors || ["button:submit"];
-	}
+    var BacksiesNoSavesies = function(changeSelectors, saveSelectors, ignoreSelectors, message) {
+        this._unsaved = false;
+        this._message = message || "Warning: You have unsaved changes on this page. If you leave your changes will be lost.";
+        this._changeSelectors = changeSelectors || [":input"];
+        this._saveSelectors = saveSelectors || ["button:submit"];
+        this._ignoreSelectors = ignoreSelectors;
+        this._debug = false;
+        return 
+    }
 
-  	/*
+    /*
      * @public
      * Sets a custom message for the warning
      */
@@ -26,35 +29,95 @@
     };
 
     /*
-     * @private
-     * Sets a the event hooks on page load
+     * @public
+     * Sets a list of elements to ignore the changeSelectors inside
      */
-	loadPage = function() {
-		for (e in this._changeSelectors) {
-			$(this._changeSelectors[e]).change(function(){ 
-				_unsaved = true;
-	       	});
-		}
+    BacksiesNoSavesies.prototype.setIgnoreSelectors = function(selectors) {
+        this._ignoreSelectors = selectors;
 
-		for (s in this._saveSelectors) {
-			$(this._saveSelectors[s]).click(function() {
-				_unsaved = false;
-			});
-		}
-	}
+        // return this for chaining
+        return this;
+    };
 
-	/*
+    /*
+     * @public
+     * Sets unsaved
+     */
+    BacksiesNoSavesies.prototype.setUnsaved = function(unsaved, ele) {
+        setUnsaved(unsaved, ele);
+
+        // return this for chaining
+        return this;
+    };
+
+    /*
+     * @public
+     * Sets debug flag on for nice logging.
+     */
+    BacksiesNoSavesies.prototype.debug = function(isDebug) {
+        this._debug = isDebug;
+        return this;
+    } 
+
+    BacksiesNoSavesies.prototype.ignoreElement = function(ele) {
+        ele.unbind("change", setUnsaved(true, ele));
+    }
+
+    /*
+     * @private
+     * Sets the event hooks on page load
+     */
+    loadPage = function() {
+        var c_selectors = this._changeSelectors;
+        var s_selectors = this._saveSelectors;
+
+        for (e in this._changeSelectors) {
+            $(this._changeSelectors[e]).change(function(){
+                setUnsaved(true, $(this))
+            });
+        }
+
+        for (s in s_selectors) {
+            $(s_selectors[s]).click(function() {
+                setUnsaved(false, $(this));
+            });
+        }
+
+        if (this._ignoreSelectors) {
+            for (i in this._ignoreSelectors) {
+                $(this._ignoreSelectors[i]).each(function(){
+                    BacksiesNoSavesies.prototype.ignoreElement($( this ))
+                });
+            }
+        }
+
+        BacksiesNoSavesies.prototype.setUnsaved(false, this);
+    }
+
+    /*
      * @private
      * Sets a the event hooks on page unload
      */
-	unloadPage = function() { 
-	    if(this._unsaved){
-	        return this._message;
-	    }
-	}
+    unloadPage = function() { 
+        if(this._unsaved){
+            return this._message;
+        }
+    }
 
-	window.onbeforeunload = unloadPage;
-	window.onload = loadPage;
-	window.BacksiesNoSavesies = BacksiesNoSavesies;
+    /*
+     * @private
+     * Sets a the _unsaved flag
+     */
+    setUnsaved = function(unsaved, ele) {
+        if (BacksiesNoSavesies.prototype.debug === true){
+            console.log(ele);
+            console.log(" is setting unsaved " + unsaved);
+        }
+        this._unsaved = unsaved;
+    }
+
+    window.onbeforeunload = unloadPage;
+    window.onload = loadPage;
+    window.BacksiesNoSavesies = BacksiesNoSavesies;
 
 })(window, document);
